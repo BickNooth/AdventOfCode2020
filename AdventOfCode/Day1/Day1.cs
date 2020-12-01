45f9f02374bb9c0e97caab8b7ab6d0a65bc9d8d5
Nicholas Booth <nicholas.booth@maples.com>
2020-12-01 11:11:35 +0000

    Day 1 part 2

--- AdventOfCode/Day1/Day1.cs
+++ AdventOfCode/Day1/Day1.cs
@@ -12,14 +12,14 @@ namespace AdventOfCode.Day1
         {
             var expenses = ReadIntsFromFile(filePath);
 
-            var numbersAddingTo2020 = FindNumbersAddingTo2020(expenses);
+            var numbersAddingTo2020 = FindThreeNumbersAddingTo2020(expenses);
 
-            var multipliedNumbers = numbersAddingTo2020.FirstNumber * numbersAddingTo2020.SecondNumber;
+            var multipliedNumbers = numbersAddingTo2020.FirstNumber * numbersAddingTo2020.SecondNumber * numbersAddingTo2020.ThirdNumber;
 
             Console.WriteLine("Multiplied result: " + multipliedNumbers);
         }
 
-        private static NumbersAddingTo2020 FindNumbersAddingTo2020(IEnumerable<int> expenses)
+        private static NumbersAddingTo2020 FindTwoNumbersAddingTo2020(IEnumerable<int> expenses)
         {
             var enumeratedArray = expenses as int[] ?? expenses.ToArray();
 
@@ -35,13 +35,36 @@ namespace AdventOfCode.Day1
             throw new NoMatchingNumbersFoundException();
         }
 
+        private static NumbersAddingTo2020 FindThreeNumbersAddingTo2020(IEnumerable<int> expenses)
+        {
+            var expensesArray = expenses as int[] ?? expenses.ToArray();
+
+            for (var firstNumberIteration = 0; firstNumberIteration < expensesArray.Length; firstNumberIteration++)
+            {
+                if (firstNumberIteration + 1 > expensesArray.Length)
+                {
+                    throw new NoMatchingNumbersFoundException();
+                }
+
+                for (var secondNumberIteration = 1; secondNumberIteration < (expensesArray.Length - 1); secondNumberIteration++)
+                {
+                    var partnerNumber = 2020 - expensesArray[firstNumberIteration] - expensesArray[secondNumberIteration];
+
+                    if (expensesArray.Contains(partnerNumber))
+                    {
+                        return new NumbersAddingTo2020(expensesArray[firstNumberIteration], expensesArray[secondNumberIteration], partnerNumber);
+                    }
+                }
+            }
+            throw new NoMatchingNumbersFoundException();
+        }
+
         private static IEnumerable<int> ReadIntsFromFile(string testInputFile)
         {
             var lineArray = File.ReadAllLines($"./Day1/{testInputFile}");
             return lineArray.Select(int.Parse);
         }
 
-        public record NumbersAddingTo2020(int FirstNumber, int SecondNumber);
-
+        public record NumbersAddingTo2020(int FirstNumber, int SecondNumber, int ThirdNumber = 0);
     }
 }
